e2f83c8573897b5744db3504c2366c56f6faa32d
shrey <shreyansh.mail@proton.me>
2026-06-28 14:26:20 +0530

    some reforms

--- go.mod
+++ go.mod
@@ -2,13 +2,10 @@ module github.com/AadamKhorShaitanBhediya/gosynth
 
 go 1.26.4
 
-require github.com/faiface/beep v1.1.0
+require github.com/gen2brain/raylib-go/raylib v0.60.0
 
 require (
-	github.com/hajimehoshi/oto v0.7.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
-	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
-	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
-	golang.org/x/sys v0.0.0-20190626150813-e07cf5db2756 // indirect
+	github.com/ebitengine/purego v0.10.1 // indirect
+	github.com/jupiterrider/ffi v0.7.0 // indirect
+	golang.org/x/exp v0.0.0-20260611194520-c48552f49976 // indirect
 )
--- main.go
+++ main.go
@@ -1,36 +1,108 @@
 package main
 
 import (
+	"fmt"
 	"math"
-	"time"
 
-	"github.com/faiface/beep"
-	"github.com/faiface/beep/speaker"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-// samples [value][channel, 2 for headphones]
-const sr float64 = 44100
+const (
+	BASE_FREQ   = 220
+	SAMPLE_RATE = 44100
+	BUFFER_SIZE = 4096
+	TEXT_SIZE   = 20
+)
 
-var t float64
+var (
+	sampleTime float32
+)
 
-func MakeNoise(samples [][2]float64) (n int, ok bool) {
-	var A float64 = 0.5
-	var f float64 = 220
-	var w = f * 2 * float64(math.Pi)
-	dt := 1 / sr
-	for i := range samples {
-		value := A * math.Sin(w*t)
-		samples[i][0] = value
-		samples[i][1] = value
-		t += dt
-	}
-	return len(samples), true
+func NoteToFreq(n uint) float32 {
+	r := math.Pow(2, float64(1)/12)
+	return BASE_FREQ * float32(math.Pow(r, float64(n)))
 }
+
+func PollNote(note *uint) {
+
+	if rl.IsKeyDown(rl.KeyOne) {
+		*note = 0
+	}
+	if rl.IsKeyDown(rl.KeyTwo) {
+		*note = 1
+	}
+	if rl.IsKeyDown(rl.KeyThree) {
+		*note = 2
+	}
+	if rl.IsKeyDown(rl.KeyFour) {
+		*note = 3
+	}
+	if rl.IsKeyDown(rl.KeyFive) {
+		*note = 4
+	}
+	if rl.IsKeyDown(rl.KeySix) {
+		*note = 5
+	}
+	if rl.IsKeyDown(rl.KeySeven) {
+		*note = 6
+	}
+	if rl.IsKeyDown(rl.KeyEight) {
+		*note = 7
+	}
+	if rl.IsKeyDown(rl.KeyNine) {
+		*note = 8
+	}
+	if rl.IsKeyDown(rl.KeyZero) {
+		*note = 9
+	}
+	if rl.IsKeyDown(rl.KeyMinus) {
+		*note = 10
+	}
+	if rl.IsKeyDown(rl.KeyEqual) {
+		*note = 11
+	}
+}
+
 func main() {
+	var (
+		note uint
+		A    float32 = 0.3
+		f    float32 = BASE_FREQ
+	)
 
-	sr := beep.SampleRate(sr)
-	speaker.Init(sr, sr.N(time.Second/10))
-	speaker.Play(beep.StreamerFunc(MakeNoise))
-	select {}
+	rl.InitWindow(800, 450, "Sound Synthesis")
+	defer rl.CloseWindow()
+	rl.SetTargetFPS(60)
 
+	rl.InitAudioDevice()
+	defer rl.CloseAudioDevice()
+
+	//number of samples to keep in memory at a time
+	rl.SetAudioStreamBufferSizeDefault(BUFFER_SIZE)
+	buffer := [BUFFER_SIZE]float32{}
+
+	//sampleSize = 32 bit floats
+	stream := rl.LoadAudioStream(SAMPLE_RATE, 32, 1)
+	rl.SetAudioStreamPan(stream, 0.0)
+	rl.PlayAudioStream(stream)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+		PollNote(&note)
+
+		if rl.IsAudioStreamProcessed(stream) {
+			for i := range buffer {
+				y := A * float32(math.Sin(2*math.Pi*float64(f*sampleTime)))
+				buffer[i] = y
+				sampleTime += 1.0 / SAMPLE_RATE
+			}
+			rl.UpdateAudioStream(stream, buffer[:]) //colon for slice
+		}
+		f = NoteToFreq(note)
+		rl.DrawText(fmt.Sprint("frequency: ", f), 0, TEXT_SIZE*0, TEXT_SIZE, rl.Red)
+		rl.DrawText(fmt.Sprint("Note: ", note), 0, TEXT_SIZE*1, TEXT_SIZE, rl.Red)
+		rl.EndDrawing()
+	}
 }
